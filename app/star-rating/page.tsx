9fc8a169ce5bc7e4d27c465b9c6d93906e52c9b3
Minhazur Rahman <minhazur.rony@gmail.com>
2024-04-14 17:47:37 +0600

    feat: add tooltip into star rating

--- app/star-rating/page.tsx
+++ app/star-rating/page.tsx
@@ -2,6 +2,15 @@
 
 import React, { useState } from "react";
 import { MdOutlineStar, MdOutlineStarBorder } from "react-icons/md";
+import { Tooltip } from "../components";
+
+const tooltipMessages = [
+  "poor",
+  "noting special",
+  "average",
+  "good",
+  "excellent",
+];
 
 export default function StarRating() {
   const stars = [1, 2, 3, 4, 5];
@@ -17,7 +26,7 @@ export default function StarRating() {
     setHoveredRating(0);
   };
 
-  const renderStar = (star: number) => {
+  const renderStar = (star: number, idx: number) => {
     if (rating >= star) {
       return (
         <MdOutlineStar
@@ -29,23 +38,32 @@ export default function StarRating() {
       );
     } else {
       return (
-        <MdOutlineStarBorder
-          key={star}
-          onClick={() => handleRatingClick(star)}
-          onMouseOver={() => setHoveredRating(star)}
-          onMouseLeave={() => setHoveredRating(0)}
-          size={"2em"}
-          className={`${hoveredRating >= star ? "cursor-pointer text-green-300" : " hover:text-green-300"}`}
-        />
+        <Tooltip content={tooltipMessages[idx]}>
+          <MdOutlineStarBorder
+            key={star}
+            onClick={() => handleRatingClick(star)}
+            onMouseEnter={() => setHoveredRating(star)}
+            onMouseLeave={() => setHoveredRating(0)}
+            size={"2em"}
+            className={`${hoveredRating >= star ? "cursor-pointer text-green-300" : " hover:text-green-300"}`}
+          />
+        </Tooltip>
       );
     }
   };
 
   return (
-    <div className="flex h-2/6 w-3/6 items-center justify-center shadow-lg">
-      {stars.map((star) => {
-        return renderStar(star);
-      })}
+    <div className="flex h-2/6 w-3/6 flex-col items-center justify-center shadow-lg">
+      <div className="flex">
+        {stars.map((star, idx) => {
+          return renderStar(star, idx);
+        })}
+      </div>
+
+      <p className="mt-6 capitalize">
+        <span className="font-bold">rating:</span>{" "}
+        {rating > 0 ? tooltipMessages[rating - 1] : "not given"}
+      </p>
     </div>
   );
 }
